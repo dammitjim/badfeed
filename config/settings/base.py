945a85a593776058180a6594f15a85c72dd6afb4
Jim Hill <jim@dammitjim.co.uk>
2018-06-03 00:40:32 +0100

    Enabling session auth in basic settings for now

--- config/settings/base.py
+++ config/settings/base.py
@@ -111,7 +111,10 @@ RQ_QUEUES = {
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
-    "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework_jwt.authentication.JSONWebTokenAuthentication"],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
     "EXCEPTION_HANDLER": "badfeed.core.exceptions.status_code_exception_handler",
     "PAGE_SIZE": 10,
 }
--- config/settings/local.py
+++ config/settings/local.py
@@ -4,5 +4,3 @@ from envparse import env
 
 
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
-if DEBUG:
-    REST_FRAMEWORK["DEFAULT_AUTHENTICATION_CLASSES"].append("rest_framework.authentication.SessionAuthentication")
