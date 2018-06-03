af37320fef4e7b66729b38f7cb3cb17597287ca2
Jim Hill <jim@dammitjim.co.uk>
2018-06-03 11:37:32 +0100

    Adding user constrained resource urls

--- badfeed/users/views.py
+++ badfeed/users/views.py
@@ -1,13 +1,14 @@
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from badfeed.core.utils import rest_message
-from badfeed.feeds.models import Feed
-from badfeed.feeds.serializers import FeedSerializer
+from badfeed.feeds.models import Feed, Entry
+from badfeed.feeds.serializers import FeedSerializer, EntrySerializer
 
 
-class WatchedFeedsList(ListAPIView):
+class MyFeedList(ListAPIView):
 
     serializer_class = FeedSerializer
 
@@ -21,3 +22,33 @@ class WatchedFeedsList(ListAPIView):
             raise Http404
         self.request.user.watch(feed, commit=True)
         return rest_message(f"You are now watching {feed.title}", status.HTTP_201_CREATED)
+
+
+class MyFeedDetail(RetrieveAPIView):
+    serializer_class = FeedSerializer
+
+    def get_object(self):
+        return get_object_or_404(Feed, slug=self.kwargs["slug"], watched_by=self.request.user)
+
+
+class MyFeedEntryList(ListAPIView):
+    serializer_class = EntrySerializer
+
+    def get_queryset(self):
+        return Entry.objects.filter(feed__slug=self.kwargs["feed_slug"], feed__watched_by=self.request.user)
+
+
+class MyFeedEntryDetail(RetrieveAPIView):
+    serializer_class = EntrySerializer
+
+    def get_object(self):
+        return get_object_or_404(
+            Entry, slug=self.kwargs["slug"], feed__slug=self.kwargs["feed_slug"], feed__watched_by=self.request.user
+        )
+
+
+class MyEntryList(ListAPIView):
+    serializer_class = EntrySerializer
+
+    def get_queryset(self):
+        return Entry.objects.filter(feed__watched_by=self.request.user)
