d9118e35b56656f191c1ab1e120acbb840bd2f68
Jim Hill <jim@dammitjim.co.uk>
2018-06-03 01:05:55 +0100

    Adding sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,1 +1,10 @@
 from .base import *  # noqa
+
+import os
+
+from envparse import env
+import raven
+
+
+INSTALLED_APPS += ("raven.contrib.django.raven_compat",)
+RAVEN_CONFIG = {"dsn": env.str("SENTRY_DSN"), "release": raven.fetch_git_sha(os.path.dirname(BASE_DIR))}
