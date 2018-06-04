5517c00e6207e0da22deab73ae30a5700593bd97
Jim Hill <jim@dammitjim.co.uk>
2018-06-04 22:13:05 +0100

    Adding CORS headers

--- config/settings/base.py
+++ config/settings/base.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # third party
+    "corsheaders",
     "rest_framework",
     "django_rq",
     # internal
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
--- config/settings/local.py
+++ config/settings/local.py
@@ -4,3 +4,4 @@ from envparse import env
 
 
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
+CORS_ORIGIN_ALLOW_ALL = True
