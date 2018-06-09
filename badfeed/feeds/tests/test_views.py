f954d9c597acab6d8113c7282c5fe7d0eb67caf4
Jim Hill <jim@dammitjim.co.uk>
2018-06-09 20:39:17 +0100

    Added a test

--- badfeed/feeds/tests/test_views.py
+++ badfeed/feeds/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
@@ -12,6 +13,11 @@ def feed():
     return mommy.make(Feed, title="My Amazing Feed!")
 
 
+@pytest.fixture
+def user():
+    return mommy.make(settings.AUTH_USER_MODEL)
+
+
 @pytest.mark.django_db
 class TestFeedList:
     @classmethod
@@ -19,6 +25,13 @@ class TestFeedList:
         cls.client = APIClient()
         cls.url = reverse("feeds:feed_list")
 
-    def test_requires_authorization(self, feed):
+    def test_requires_authorization(self):
+        """When logged out, the endpoint should respond unauthorized."""
         response = self.client.get(self.url)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_responds_200_when_logged_in(self, user):
+        """When logged in, the endpoint should respond 200."""
+        self.client.force_authenticate(user=user)
+        response = self.client.get(self.url)
+        assert response.status_code == status.HTTP_200_OK
