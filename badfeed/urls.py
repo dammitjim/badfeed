9da99f3b51569b51676e3b10a26bf4f4a7fec41d
Jim Hill <jim@dammitjim.co.uk>
2018-12-29 22:48:15 +0000

    Adding state-specific list views

--- badfeed/urls.py
+++ badfeed/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("admin/django-rq/", include("django_rq.urls")),
-    path("f/", include("badfeed.feeds.urls")),
+    path("", include("badfeed.feeds.urls")),
     path("", include("badfeed.users.urls")),
 ]
