79b0eded86a3c5c9bc19fe0ac39cda2581723803
Jim Hill <jim@dammitjim.co.uk>
2019-01-16 22:56:01 +0000

    Webpack is a fucking crime

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,27 +1,45 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
-module.exports = {
-    baseUrl: "http://0.0.0.0:8080/",
-    outputDir: "./dist/",
+if (process.env.PRODUCTION == "true") {
+    module.exports = {
+        outputDir: "./dist/entry-widget/",
+        publicPath: '/static',
 
-    chainWebpack: config => {
-        config.optimization.splitChunks(false);
+        chainWebpack: config => {
+            config.optimization.splitChunks(false);
+            config
+                .plugin("BundleTracker")
+                .use(BundleTracker, [
+                    { filename: "../frontend/webpack-stats-prod.json" },
+                ]);
+            return config;
+        }
+    }
 
-        config
-            .plugin("BundleTracker")
-            .use(BundleTracker, [
-                { filename: "../frontend/webpack-stats.json" },
-            ]);
+} else {
+    module.exports = {
+        baseUrl: "http://0.0.0.0:8080/",
+        outputDir: "./dist/",
 
-        config.resolve.alias.set("__STATIC__", "static");
+        chainWebpack: config => {
+            config.optimization.splitChunks(false);
 
-        config.devServer
-            .public("http://0.0.0.0:8080")
-            .host("0.0.0.0")
-            .port(8080)
-            .hotOnly(true)
-            .watchOptions({ poll: 1000 })
-            .https(false)
-            .headers({ "Access-Control-Allow-Origin": ["*"] });
-    },
-};
+            config
+                .plugin("BundleTracker")
+                .use(BundleTracker, [
+                    { filename: "../frontend/webpack-stats.json" },
+                ]);
+
+            config.resolve.alias.set("__STATIC__", "static");
+
+            config.devServer
+                .public("http://0.0.0.0:8080")
+                .host("0.0.0.0")
+                .port(8080)
+                .hotOnly(true)
+                .watchOptions({ poll: 1000 })
+                .https(false)
+                .headers({ "Access-Control-Allow-Origin": ["*"] });
+        },
+    };
+}
