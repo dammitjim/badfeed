7eba1661ba4f869e7d864e6db18e96335e311579
Jim Hill <jim@dammitjim.co.uk>
2019-02-08 23:52:41 +0000

    Removing now unused API endpoint for managing entry state

--- badfeed/feeds/api/urls.py
+++ badfeed/feeds/api/urls.py
@@ -6,11 +6,6 @@ from badfeed.feeds.api import views
 app_name = "api_feeds"
 urlpatterns = [
     path("entries/unread/", views.UnreadEntryList.as_view(), name="entries_unread"),
-    path(
-        "entries/<int:pk>/",
-        views.EntryStateManagerView.as_view(),
-        name="entries_state_manager",
-    ),
     path("dash/", views.GenericFeedDashboardView.as_view(), name="dash"),
     path("state/", views.EntryStateCreationView.as_view(), name="state"),
 ]
