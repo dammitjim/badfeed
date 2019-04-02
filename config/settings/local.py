8963f04f2d6f19eb436499372ccbcff1c4882d87
Jim Hill <jim@dammitjim.co.uk>
2019-04-03 00:15:27 +0100

    Sorting out a bunch of settings for production config

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,7 +4,4 @@ from .base import *  # noqa
 
 
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
-CORS_ORIGIN_ALLOW_ALL = True
-INSTALLED_APPS += ["corsheaders"]
-MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
 RQ_ENABLED = env.bool("RQ_ENABLED", default=False)
--- config/settings/production.py
+++ config/settings/production.py
@@ -13,11 +13,4 @@ sentry_init(
 
 POCKET_CONSUMER_KEY = env.str("POCKET_CONSUMER_KEY")
 
-STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, "dist", "entry-widget")]
-WEBPACK_LOADER = {
-    "DEFAULT": {
-        "CACHE": DEBUG,
-        "BUNDLE_DIR_NAME": "/entry-widget/",  # must end with slash
-        "STATS_FILE": os.path.join(FRONTEND_DIR, "webpack-stats-prod.json"),
-    }
-}
+STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, "dist", "assets")]
