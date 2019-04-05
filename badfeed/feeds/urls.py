61a6bd0e0819d7b4236149752dc8d59bdae7d6b0
Jim Hill <jim@dammitjim.co.uk>
2019-04-05 13:45:28 +0100

    Adding basic entry detail view

--- badfeed/feeds/urls.py
+++ badfeed/feeds/urls.py
@@ -11,66 +11,77 @@ urlpatterns = [
     path("pinned/", lists.PinnedEntriesListView.as_view(), name="pinned_entries"),
     path("saved/", lists.SavedEntriesListView.as_view(), name="saved_entries"),
     path("archived/", lists.ArchivedEntriesListView.as_view(), name="archived_entries"),
+    path("f/<slug:slug>/", details.FeedDetailView.as_view(), name="detail"),
+    path(
+        "f/<slug:feed_slug>/<slug:entry_slug>/",
+        details.EntryDetailView.as_view(),
+        name="entry_detail",
+    ),
+]
+
+# TODO put some thought into these urls as they ain't great
+action_urls = [
+    path("fa/<slug:slug>/actions/", actions.FeedActionsView.as_view(), name="actions"),
+    path(
+        "fa/<slug:slug>/watch/",
+        actions.FeedWatchToggleView.as_view(should_toggle=True),
+        name="watch",
+    ),
+    path(
+        "fa/<slug:slug>/unwatch/",
+        actions.FeedWatchToggleView.as_view(should_toggle=False),
+        name="unwatch",
+    ),
+    path(
+        "fa/<slug:feed_slug>/<slug:entry_slug>/read/",
+        redirects.EntryOffloadView.as_view(),
+        name="entry_read",
+    ),
+    path(
+        "fa/<slug:feed_slug>/<slug:entry_slug>/pin/",
+        actions.EntryPinToggleView.as_view(should_toggle=True),
+        name="entry_pin",
+    ),
+    path(
+        "fa/<slug:feed_slug>/<slug:entry_slug>/unpin/",
+        actions.EntryPinToggleView.as_view(should_toggle=False),
+        name="entry_unpin",
+    ),
+    path(
+        "fa/<slug:feed_slug>/<slug:entry_slug>/save/",
+        actions.EntrySaveToggleView.as_view(should_toggle=True),
+        name="entry_save",
+    ),
+    path(
+        "fa/<slug:feed_slug>/<slug:entry_slug>/unsave/",
+        actions.EntrySaveToggleView.as_view(should_toggle=False),
+        name="entry_unsave",
+    ),
+    path(
+        "fa/<slug:feed_slug>/<slug:entry_slug>/delete/",
+        actions.EntryDeleteToggleView.as_view(should_toggle=True),
+        name="entry_delete",
+    ),
+    path(
+        "fa/<slug:feed_slug>/<slug:entry_slug>/undelete/",
+        actions.EntryDeleteToggleView.as_view(should_toggle=False),
+        name="entry_undelete",
+    ),
+    path(
+        "fa/<slug:feed_slug>/<slug:entry_slug>/actions/",
+        actions.EntryActionsView.as_view(),
+        name="entry_actions",
+    ),
+    path(
+        "fa/<slug:feed_slug>/<slug:entry_slug>/pocket/",
+        actions.SaveEntryToPocketView.as_view(),
+        name="entry_pocket",
+    ),
     path(
         "delete-multi/",
         actions.MultiEntryDeleteView.as_view(),
         name="entry_multi_delete",
     ),
-    path("f/<slug:slug>/", details.FeedDetailView.as_view(), name="detail"),
-    path("f/<slug:slug>/actions/", actions.FeedActionsView.as_view(), name="actions"),
-    path(
-        "f/<slug:slug>/watch/",
-        actions.FeedWatchToggleView.as_view(should_toggle=True),
-        name="watch",
-    ),
-    path(
-        "f/<slug:slug>/unwatch/",
-        actions.FeedWatchToggleView.as_view(should_toggle=False),
-        name="unwatch",
-    ),
-    path(
-        "f/<slug:feed_slug>/<slug:entry_slug>/read/",
-        redirects.EntryOffloadView.as_view(),
-        name="entry_read",
-    ),
-    path(
-        "f/<slug:feed_slug>/<slug:entry_slug>/pin/",
-        actions.EntryPinToggleView.as_view(should_toggle=True),
-        name="entry_pin",
-    ),
-    path(
-        "f/<slug:feed_slug>/<slug:entry_slug>/unpin/",
-        actions.EntryPinToggleView.as_view(should_toggle=False),
-        name="entry_unpin",
-    ),
-    path(
-        "f/<slug:feed_slug>/<slug:entry_slug>/save/",
-        actions.EntrySaveToggleView.as_view(should_toggle=True),
-        name="entry_save",
-    ),
-    path(
-        "f/<slug:feed_slug>/<slug:entry_slug>/unsave/",
-        actions.EntrySaveToggleView.as_view(should_toggle=False),
-        name="entry_unsave",
-    ),
-    path(
-        "f/<slug:feed_slug>/<slug:entry_slug>/delete/",
-        actions.EntryDeleteToggleView.as_view(should_toggle=True),
-        name="entry_delete",
-    ),
-    path(
-        "f/<slug:feed_slug>/<slug:entry_slug>/undelete/",
-        actions.EntryDeleteToggleView.as_view(should_toggle=False),
-        name="entry_undelete",
-    ),
-    path(
-        "f/<slug:feed_slug>/<slug:entry_slug>/actions/",
-        actions.EntryActionsView.as_view(),
-        name="entry_actions",
-    ),
-    path(
-        "f/<slug:feed_slug>/<slug:entry_slug>/pocket/",
-        actions.SaveEntryToPocketView.as_view(),
-        name="entry_pocket",
-    ),
 ]
+
+urlpatterns += action_urls
--- badfeed/feeds/views/details.py
+++ badfeed/feeds/views/details.py
@@ -1,10 +1,30 @@
 """A detail view displays individual content."""
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView
 
-from badfeed.feeds.models import Feed
+from badfeed.feeds.models import Entry, Feed
 
 
 class FeedDetailView(LoginRequiredMixin, DetailView):
     model = Feed
-    template_name = "feeds/detail.html"
+    template_name = "feeds/feed_detail.html"
+
+
+class EntryDetailView(LoginRequiredMixin, DetailView):
+    context_object_name = "entry"
+    model = Entry
+    template_name = "feeds/entry_detail.html"
+
+    def get_object(self, queryset=None):
+        """Load object based on feed and entry slugs."""
+        return get_object_or_404(
+            Entry, slug=self.kwargs["entry_slug"], feed__slug=self.kwargs["feed_slug"]
+        )
+
+    def get_context_data(self, **kwargs):
+        """Load page title into context."""
+        ctx = super().get_context_data(**kwargs)
+        ctx["page_title"] = self.object.title
+        ctx["page_subtitle"] = f"{self.object.feed.title}"
+        return ctx
