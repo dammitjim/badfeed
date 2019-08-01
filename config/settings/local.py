4f667418e428d1c698d76bbe3c72a69f2c9a5fa0
Jim Hill <jim@tightenupthe.tech>
2019-08-02 00:14:50 +0100

    Overriding webpack stats file used for local dev

--- config/settings/local.py
+++ config/settings/local.py
@@ -6,3 +6,7 @@ from .base import *  # noqa
 ALLOWED_HOSTS = env.bool("DJANGO_ALLOWED_HOSTS", default=["*"])
 DEBUG = env.bool("DJANGO_DEBUG", default=True)
 RQ_ENABLED = env.bool("RQ_ENABLED", default=False)
+
+WEBPACK_LOADER["DEFAULT"]["STATS_FILE"] = os.path.join(
+    FRONTEND_DIR, "webpack-stats-dev.json"
+)
