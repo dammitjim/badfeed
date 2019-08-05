2b63835d0bac7c14dca23bfc034c9a2b2411eb3c
Jim Hill <jim@tightenupthe.tech>
2019-08-05 23:29:06 +0100

    Bunch of new stuff

---
+++ frontend/new/src/reader/components/ButtonPin.vue
@@ -0,0 +1,27 @@
+<template>
+    <a class="button-pin button" @click="pin">
+        <span class="icon is-small">
+            <i class="fas fa-thumbtack"></i>
+        </span>
+        <span>Pin</span>
+    </a>
+</template>
+<script>
+import { ACTIONS } from "@/reader/store/main.js";
+
+export default {
+    name: "ButtonPin",
+    props: {
+        entry: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        async pin() {
+            await this.$store.dispatch(ACTIONS.PIN_STORY, this.entry)
+        }
+    }
+}
+</script>
+
---
+++ frontend/new/src/reader/components/EntryList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="entry-list">
+    <transition-group name="entry-list-transition">
+      <EntryRow v-for="entry in entries" :key="entry.id" :entry="entry" />
+    </transition-group>
+  </div>
+</template>
+<script>
+import EntryRow from "@/reader/components/EntryRow";
+export default {
+    name: "EntryList",
+    components: {
+        EntryRow
+    },
+    props: {
+        entries: {
+            type: Array,
+            required: true
+        }
+    }
+};
+</script>
+<style lang="scss">
+@import url("../../styles/components/EntryList.scss");
+</style>
+
+
