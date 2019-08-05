1d68a96eb3083ae4b2f2155fe18a8e292d9ac4a9
Jim Hill <jim@tightenupthe.tech>
2019-08-05 22:14:19 +0100

    Starting work on some basic components using Buefy

--- frontend/new/src/reader/App.vue
+++ frontend/new/src/reader/App.vue
@@ -1,29 +1,33 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link>|
-            <router-link to="/about">About</router-link>
+    <div class="section">
+        <div class="app container is-fluid">
+            <section class="columns">
+                <div class="column is-one-fifth"></div>
+                <div class="column is-three-fifths">
+                    <TabbedRouter />
+                </div>
+            </section>
+            <section class="columns">
+                <div class="column is-one-fifth">
+                    Feeds
+                </div>
+                <div class="column is-three-fifths">
+                    <router-view />
+                </div>
+            </section>
         </div>
-        <router-view />
     </div>
 </template>
+<script>
+import TabbedRouter from "@/reader/components/TabbedRouter";
 
-<style lang="scss">
-#app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-#nav {
-    padding: 30px;
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-        &.router-link-exact-active {
-            color: #42b983;
-        }
+export default {
+    name: "App",
+    components: {
+        TabbedRouter
     }
 }
+</script>
+<style lang="scss" scoped>
+@import url("../styles/views/app.scss");
 </style>
