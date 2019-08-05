10297087f8ac0eb8ede4d6ce88903e97394794cd
Jim Hill <jim@tightenupthe.tech>
2019-08-05 22:14:43 +0100

    New components!

---
+++ frontend/new/src/reader/components/TabbedRouter.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-tabs type="is-boxed" @change="handleClick">
+    <b-tab-item label="Inbox" icon="paper-plane"></b-tab-item>
+    <b-tab-item label="Pinned" icon="thumbtack"></b-tab-item>
+  </b-tabs>
+</template>
+<script>
+const indexToRouteMap = {
+    0: { path: "/" },
+    1: { path: "pinned" }
+};
+
+// TODO: calculate starting index based on current route
+export default {
+    name: "TabbedRouter",
+    methods: {
+        /**
+         * Navigate to the corresponding route based on the
+         * tab index.
+         *
+         * @param {Integer} index
+         */
+        handleClick(index) {
+            const routeProps = indexToRouteMap[index];
+            this.$router.push(routeProps);
+        }
+    }
+};
+</script>
+
---
+++ frontend/new/src/reader/views/Inbox.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="view-inbox">
+        Test
+    </div>
+</template>
+<script>
+export default {
+    name: "Inbox"
+}
+</script>
+<style lang="scss" scoped>
+@import url("../../styles/views/inbox.scss");
+</style>
