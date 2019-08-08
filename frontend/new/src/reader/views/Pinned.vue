860d1649944e1e721dc36cf9ffcdfd46f24a561d
Jim Hill <jim@tightenupthe.tech>
2019-08-09 00:10:33 +0100

    Updating to python 3.7, formatting, docker config

--- frontend/new/src/reader/views/Pinned.vue
+++ frontend/new/src/reader/views/Pinned.vue
@@ -1,36 +1,32 @@
 <template>
-  <div class="view-pinned">
-    <div
-        class="box"
-        v-for="story in stories"
-        :key="story.id"
-    >
-        <div class="level">
-            <div>
-                <p>{{ story.title }}</p>
-                <p>{{ story.feed.name }} - {{ story.posted }}</p>
-            </div>
-            <div class="field is-grouped">
-                <p class="control">
-                    <a class="button">
-                        <span class="icon is-small">
-                            <i class="fas fa-thumbtack"></i>
-                        </span>
-                        <span>Pin</span>
-                    </a>
-                </p>
-                <p class="control">
-                    <a class="button">
-                        <span class="icon is-small">
-                            <i class="fas fa-archive"></i>
-                        </span>
-                        <span>Done</span>
-                    </a>
-                </p>
+    <div class="view-pinned">
+        <div class="box" v-for="story in stories" :key="story.id">
+            <div class="level">
+                <div>
+                    <p>{{ story.title }}</p>
+                    <p>{{ story.feed.name }} - {{ story.posted }}</p>
+                </div>
+                <div class="field is-grouped">
+                    <p class="control">
+                        <a class="button">
+                            <span class="icon is-small">
+                                <i class="fas fa-thumbtack"></i>
+                            </span>
+                            <span>Pin</span>
+                        </a>
+                    </p>
+                    <p class="control">
+                        <a class="button">
+                            <span class="icon is-small">
+                                <i class="fas fa-archive"></i>
+                            </span>
+                            <span>Done</span>
+                        </a>
+                    </p>
+                </div>
             </div>
         </div>
     </div>
-  </div>
 </template>
 <script>
 export default {
