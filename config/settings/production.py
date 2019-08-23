2a4bd5adda5373cbb107261c4820e2b5c7819357
Jim Hill <jim@tightenupthe.tech>
2019-08-23 23:32:09 +0100

    Updating sentry sdk

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,7 @@
 from envparse import env
 from sentry_sdk import init as sentry_init
 from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 from sentry_sdk.integrations.rq import RqIntegration
 
 from .base import *  # noqa
@@ -8,7 +9,7 @@ from .base import *  # noqa
 
 sentry_init(
     dsn=env.str("SENTRY_DSN", default=""),
-    integrations=[DjangoIntegration(), RqIntegration()],
+    integrations=[DjangoIntegration(), RedisIntegration(), RqIntegration()],
 )
 
 POCKET_CONSUMER_KEY = env.str("POCKET_CONSUMER_KEY")
